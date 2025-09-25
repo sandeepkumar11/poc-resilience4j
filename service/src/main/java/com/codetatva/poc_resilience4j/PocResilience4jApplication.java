439ed58ed6c4e62c36477f3cf014ccf3a898e011
sandeep_kumar <sandeeplavaniya7@gmail.com>
2025-09-25 23:09:13 +0530

    add the sample sb service

---
+++ service/src/main/java/com/codetatva/poc_resilience4j/PocResilience4jApplication.java
@@ -0,0 +1,13 @@
+package com.codetatva.poc_resilience4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocResilience4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocResilience4jApplication.class, args);
+	}
+
+}
---
+++ service/src/main/java/com/codetatva/poc_resilience4j/exception/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.codetatva.poc_resilience4j.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
